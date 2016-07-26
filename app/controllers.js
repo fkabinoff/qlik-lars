fc8ef69add870081fc33563ce961cdb074ad7ff3
Francis Kabinoff <francis.kabinoff@gmail.com>
2016-07-25 22:07:17 -0400

    Update controllers.js

--- app/controllers.js
+++ app/controllers.js
@@ -1,16 +1,16 @@
 define([], function() {
 
-    var controllers = {};
+	var controllers = {};
 
-    controllers.appController = function($scope, qlikApp) {
+	controllers.appController = function($scope, qlikApp) {
 
-        // gets qlik sense object and adds to #chart1
-        qlikApp.app.getObject("chart1", "a5e0f12c-38f5-4da9-8f3f-0e4566b28398").then(function(model) {
-            qlikApp.addSenseObject(model); // add sense object to objects to be closed on route change
-        });
+		// gets qlik sense object and adds to #chart1
+		qlikApp.app.getObject("chart1", "a5e0f12c-38f5-4da9-8f3f-0e4566b28398").then(function(model) {
+			qlikApp.addSenseObject(model); // add sense object to objects to be closed on route change
+		});
 
 	};
-    controllers.appController.$inject = ['$scope', 'qlikApp'];
+	controllers.appController.$inject = ['$scope', 'qlikApp'];
 
-    return controllers;
+	return controllers;
 });
